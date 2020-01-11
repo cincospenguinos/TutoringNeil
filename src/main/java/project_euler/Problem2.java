93025c2db6a4395bf243b4131a25073a715ae8ca
Andre LaFleur <andre.lafleur@utah.edu>
2020-01-10 19:55:44 -0700

    Solve Problem 2

--- src/main/java/project_euler/Problem2.java
+++ src/main/java/project_euler/Problem2.java
@@ -12,6 +12,24 @@ package project_euler;
  */
 public class Problem2 {
     public static void main(String[] args) {
+        int sum = 0;
+        int a = 1;
+        int b = 2;
 
+        while(b < 4000000) {
+            if (b % 2 == 0) {
+                sum += b;
+            }
+
+            int c = nextFibo(a, b);
+            a = b;
+            b = c;
+        }
+
+        System.out.println(sum);
+    }
+
+    public static int nextFibo(int a, int b) {
+        return a + b;
     }
 }
