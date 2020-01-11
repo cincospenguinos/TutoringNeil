47f2d5c620dd881b0d09c275ebe0cfec9720152c
Andre LaFleur <andre.lafleur@utah.edu>
2020-01-10 20:08:12 -0700

    Speed up solution Problem 2

--- src/main/java/project_euler/Problem2.java
+++ src/main/java/project_euler/Problem2.java
@@ -12,24 +12,22 @@ package project_euler;
  */
 public class Problem2 {
     public static void main(String[] args) {
-        int sum = 0;
-        int a = 1;
-        int b = 2;
+        int sum = 2;
+        int a = 2;
+        int b = 8;
 
         while(b < 4000000) {
-            if (b % 2 == 0) {
-                sum += b;
-            }
+            sum += b;
 
             int c = nextFibo(a, b);
             a = b;
             b = c;
         }
 
-        System.out.println(sum);
+        System.out.println(sum); // 4613732
     }
 
     public static int nextFibo(int a, int b) {
-        return a + b;
+        return 4 * b + a;
     }
 }
